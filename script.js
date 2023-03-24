cbb9dd42bec794baadfb5fcf9bd134b027028f19
Warren Coffey <warrencoffey@googlemail.com>
2023-03-24 14:37:53 +0000

    Create operate function

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ function multiply(a, b) {
 function divide(a, b) {
     console.log(a / b);
 }
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "add":
+            add(a, b);
+            break;
+
+        case "subtract":
+            subtract(a, b);
+            break;
+
+        case "multiply":
+            multiply(a, b);
+            break;
+
+        case "divide":
+            divide(a, b);
+            break;
+    }
+}
